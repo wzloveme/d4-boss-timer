640c881ce56aea9a1d74e4141377cba3534e0622
wzloveme <54339932+wzloveme@users.noreply.github.com>
2025-12-30 21:27:36 +1030

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,29 +1,32 @@
-let notifyTimer = null;
-
 self.addEventListener("message", event => {
   if (event.data.type === "schedule") {
-    if (notifyTimer) clearTimeout(notifyTimer);
-
-    const interval = 105 * 60 * 1000;
-    const lastBoss = event.data.lastBoss;
-    const before = event.data.before * 60 * 1000;
-
-    let now = Date.now();
-    let nextBoss = lastBoss + interval;
-    while (nextBoss <= now) {
-      nextBoss += interval;
-    }
-
-    let notifyTime = nextBoss - before;
-    let delay = notifyTime - now;
-
-    if (delay > 0) {
-      notifyTimer = setTimeout(() => {
-        self.registration.showNotification("暗黑 4 世界 Boss 即将刷新", {
-          body: `还有 ${event.data.before} 分钟`,
-          icon: "icon.png"
-        });
-      }, delay);
-    }
+    schedule(event.data.lastBoss, event.data.before);
   }
 });
+
+function schedule(lastBoss, beforeMinutes) {
+  const interval = 105 * 60 * 1000;
+  const before = beforeMinutes * 60 * 1000;
+
+  let now = Date.now();
+  let nextBoss = lastBoss + interval;
+
+  while (nextBoss <= now) {
+    nextBoss += interval;
+  }
+
+  let notifyTime = nextBoss - before;
+  let delay = notifyTime - now;
+
+  if (delay > 0) {
+    setTimeout(() => {
+      self.registration.showNotification("暗黑 4 世界 Boss", {
+        body: `还有 ${beforeMinutes} 分钟刷新`,
+        icon: "icon.png"
+      });
+
+      // 🔄 自动安排下一次
+      schedule(nextBoss, beforeMinutes);
+    }, delay);
+  }
+}
