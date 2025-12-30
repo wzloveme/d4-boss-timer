b99c830833607729a40bcb97f08a191c81f776a4
wzloveme <54339932+wzloveme@users.noreply.github.com>
2025-12-30 12:25:58 +1030

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,16 +1,29 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("d4-boss").then(cache => {
-      return cache.addAll([
-        "index.html",
-        "manifest.json"
-      ]);
-    })
-  );
-});
+let notifyTimer = null;
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+self.addEventListener("message", event => {
+  if (event.data.type === "schedule") {
+    if (notifyTimer) clearTimeout(notifyTimer);
+
+    const interval = 105 * 60 * 1000;
+    const lastBoss = event.data.lastBoss;
+    const before = event.data.before * 60 * 1000;
+
+    let now = Date.now();
+    let nextBoss = lastBoss + interval;
+    while (nextBoss <= now) {
+      nextBoss += interval;
+    }
+
+    let notifyTime = nextBoss - before;
+    let delay = notifyTime - now;
+
+    if (delay > 0) {
+      notifyTimer = setTimeout(() => {
+        self.registration.showNotification("暗黑 4 世界 Boss 即将刷新", {
+          body: `还有 ${event.data.before} 分钟`,
+          icon: "icon.png"
+        });
+      }, delay);
+    }
+  }
 });
